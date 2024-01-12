0880c3f1d4ae6b85e08afb920e3f1a25a74f376b
KarlGilden <karl.gildenhuys@gmail.com>
2024-01-13 06:45:56 +1300

    fix phrase list dialog on mobile

--- leo-lang-lite/src/components/PhraseListDialog.tsx
+++ leo-lang-lite/src/components/PhraseListDialog.tsx
@@ -11,14 +11,14 @@ interface IProps {
 const PhraseListDialog = ({open, setOpen, phraseList, exportFile}:IProps) => {
   return (
     <div className={`${open ? "block":"hidden"} flex justify-center items-center h-content fixed top-0 bottom-0 right-0 left-0 bg-black-blur`}>
-        <div className="bg-[#fff] rounded-[5px] w-[50%]">
+        <div className="bg-[#fff] rounded-[5px] sm:w-[50%] w-[90%]">
             <div className="dialog-header p-5 flex items-center">
                 <button onClick={()=>{setOpen(false)}} className="p-2 text-2xl"><MdClose /></button>
                 <p className="p-2"></p>
                 <button onClick={()=>{exportFile()}} className="px-4 py-1 bg-[#000] text-[#fff] rounded-full">Export to Anki</button>
             </div>
             <div className="dialog-content p-5">
-                <table>
+                <table className="w-full">
                     <thead>
                         <tr>
                             <td className="text-left p-2 border-[1px]">Original</td>
